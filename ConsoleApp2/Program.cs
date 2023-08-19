369cbbaa46df6a883b6d1e7894672402112ed041
Leonid <enykoruna1@gmail.com>
2023-08-19 15:05:06 +0300

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp2;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 10;
+        for (int i = 0; i < a; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
